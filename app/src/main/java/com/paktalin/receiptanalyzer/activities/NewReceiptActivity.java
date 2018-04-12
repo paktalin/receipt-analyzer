d6bba9438f1c2a0d8b3b7b481b8b99ac9ad1e060
Paktalin <likorn@ttu.ee>
2018-04-12 22:59:20 +0300

    Added activity NewReceiptActivity

--- app/src/main/java/com/paktalin/receiptanalyzer/activities/NewReceiptActivity.java
+++ app/src/main/java/com/paktalin/receiptanalyzer/activities/NewReceiptActivity.java
@@ -1,8 +1,23 @@
 package com.paktalin.receiptanalyzer.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.paktalin.receiptanalyzer.R;
+
 /**
  * Created by Paktalin on 12/04/2018.
  */
 
-public class NewReceiptActivity {
+public class NewReceiptActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_receipt);
+        TextView textView = findViewById(R.id.receipt_info);
+
+
+    }
 }
