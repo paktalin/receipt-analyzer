bab1441c673658c2eea97c11e3b1e344d660bf3a
Paktalin <likorn@ttu.ee>
2018-04-13 18:10:19 +0300

    Progress bar works correctly

--- app/src/main/java/com/paktalin/receiptanalyzer/activities/NewReceiptActivity.java
+++ app/src/main/java/com/paktalin/receiptanalyzer/activities/NewReceiptActivity.java
@@ -3,9 +3,12 @@ package com.paktalin.receiptanalyzer.activities;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,42 +26,56 @@ import java.io.FileNotFoundException;
 public class NewReceiptActivity extends AppCompatActivity {
     private static final String TAG = NewReceiptActivity.class.getSimpleName();
 
-    Uri imageUri;
     Bitmap bitmap;
     Receipt receipt;
+    ProgressBar progress;
+    TextView textViewSupermarket, textViewRetailer, textViewAddress, textViewFinalPrice;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_receipt);
-
-        ProgressBar progress = findViewById(R.id.progress_bar);
-        progress.setIndeterminate(true);
-
-        TextView textViewSupermarket = findViewById(R.id.supermarket);
-        TextView textViewRetailer = findViewById(R.id.retailer);
-        TextView textViewAddress = findViewById(R.id.address);
-        TextView textViewFinalPrice = findViewById(R.id.final_price);
-
-        int rotation = getIntent().getIntExtra("rotation", 0);
-        imageUri = getIntent().getParcelableExtra("uri");
-        extractBitmap(rotation);
-
-        Thread thread = new Thread(() -> {
-            receipt = ReceiptExtractor.extract(NewReceiptActivity.this, bitmap);
-        });
-        thread.start();
-        while(thread.isAlive());
-        textViewSupermarket.setText(receipt.getSupermarket());
-        textViewRetailer.setText(receipt.getRetailer());
-        textViewAddress.setText(receipt.getAddress());
-        String finalPrice = String.valueOf(receipt.getFinalPrice());
-        textViewFinalPrice.setText(finalPrice);
+        MyAsyncTask task = new MyAsyncTask();
+        task.execute();
     }
 
-    private void extractBitmap(int rotation) {
+    class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            int rotation = getIntent().getIntExtra("rotation", 0);
+            Uri imageUri = getIntent().getParcelableExtra("uri");
+            extractBitmap(rotation, imageUri);
+            receipt = ReceiptExtractor.extract(NewReceiptActivity.this, bitmap);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress = findViewById(R.id.progress_bar);
+            progress.setIndeterminate(true);
+            progress.setVisibility(View.VISIBLE);
+            textViewSupermarket = findViewById(R.id.supermarket);
+            textViewRetailer = findViewById(R.id.retailer);
+            textViewAddress = findViewById(R.id.address);
+            textViewFinalPrice = findViewById(R.id.final_price);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progress.setVisibility(View.INVISIBLE);
+            textViewSupermarket.setText(receipt.getSupermarket());
+            textViewRetailer.setText(receipt.getRetailer());
+            textViewAddress.setText(receipt.getAddress());
+            String finalPrice = String.valueOf(receipt.getFinalPrice());
+            textViewFinalPrice.setText(finalPrice);
+        }
+    }
+
+    private void extractBitmap(int rotation, Uri uri) {
         try {
-            bitmap = FileManager.decodeBitmapUri(NewReceiptActivity.this, imageUri);
+            bitmap = FileManager.decodeBitmapUri(NewReceiptActivity.this, uri);
             Matrix matrix = new Matrix();
             matrix.postRotate(rotation);
             bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
