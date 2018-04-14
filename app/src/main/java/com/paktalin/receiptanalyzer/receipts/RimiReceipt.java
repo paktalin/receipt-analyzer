2c8a814e948e8a71f5539d55e894f4a93fca3d30
Paktalin <likorn@ttu.ee>
2018-04-14 21:55:54 +0300

    Calculates Rimi final price

--- app/src/main/java/com/paktalin/receiptanalyzer/receipts/RimiReceipt.java
+++ app/src/main/java/com/paktalin/receiptanalyzer/receipts/RimiReceipt.java
@@ -15,12 +15,13 @@ public class RimiReceipt extends Receipt {
         super(lines);
         purchasesStart = startLine("klient", 6);
         purchasesEnd = endLine("kaardimakse", false);
+        calculateFinalPrice();
     }
 
     @Override
     int startLine(String startString, int number) {
         String string = StringManager.clean(lines[number]);
-        string = string.substring(0, 7);
+        string = string.substring(0, startString.length());
         if (StringManager.similar(string, startString))
             return number + 1;
         return -1;
@@ -30,5 +31,26 @@ public class RimiReceipt extends Receipt {
     public String cutRetailersLine(String line) {
         Log.d(TAG, line.substring(4, line.length()));
         return line.substring(4, line.length());
+    }
+
+    @Override
+    void calculateFinalPrice() {
+        finalPrice = getPayment("kokku");
+    }
+
+    private float getPayment(String payment) {
+        float price = -1;
+        for (int i = lines.length - 1; i > purchasesEnd; i--) {
+            String line = lines[i];
+            try {
+                String cut = line.substring(0, payment.length());
+                if (StringManager.similar(cut, payment)) {
+                    price = StringManager.extractFloat(line);
+                    break;
+                }
+            }catch (StringIndexOutOfBoundsException ignored) {
+            }
+        }
+        return price;
     }
 }
