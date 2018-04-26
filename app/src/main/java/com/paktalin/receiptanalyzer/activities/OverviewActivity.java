e23e869c59254095c3b2288f87b277db62072867
Paktalin <likorn@ttu.ee>
2018-04-26 23:42:53 +0300

    Represents expenses for different periods

--- app/src/main/java/com/paktalin/receiptanalyzer/activities/OverviewActivity.java
+++ app/src/main/java/com/paktalin/receiptanalyzer/activities/OverviewActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.paktalin.receiptanalyzer.R;
@@ -25,16 +28,45 @@ public class OverviewActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
-        ((TextView)findViewById(R.id.expenses)).setText(String.valueOf(getExpenses()));
+
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setSelection(1);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String expenses;
+                switch (position) {
+                    case 0:
+                        expenses = "You spent " + getExpenses(7776000000L) + "€ ";
+                        break;
+                    case 2:
+                        expenses = "You spent " + getExpenses(1209600000L) + "€ ";
+                        break;
+                    case 3:
+                        expenses = "You spent " + getExpenses(604800000L) + "€ ";
+                        break;
+                    default:
+                        expenses = "You spent " + getExpenses(2592000000L) + "€ ";
+                        break;
+                }
+
+                ((TextView)findViewById(R.id.expenses)).setText(expenses);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        String expenses = "You spent " + getExpenses(0) + "€ in the last 30 days";
+        ((TextView)findViewById(R.id.expenses)).setText(expenses);
     }
 
-    private float getExpenses() {
+    private float getExpenses(long period) {
         DatabaseHelper helper = new DatabaseHelper(OverviewActivity.this);
         SQLiteDatabase db = helper.getReadableDatabase();
 
         long currentTime = System.currentTimeMillis();
-        long month = 2592000000L;
-        long startingTime = currentTime - month;
+        long startingTime = currentTime - period;
 
         String selection = COLUMN_DATE + ", " + COLUMN_FINAL_PRICE;
         String query = "SELECT " + selection + " FROM " + TABLE_NAME_RECEIPT + " WHERE " + COLUMN_DATE + " BETWEEN "
