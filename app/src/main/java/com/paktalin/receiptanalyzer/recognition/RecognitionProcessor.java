fa0f5904624b1084f46879e8f68512572a3817d1
Paktalin <likorn@ttu.ee>
2018-07-01 14:08:12 +0300

    Imported Google Cloud Vision

---
+++ app/src/main/java/com/paktalin/receiptanalyzer/recognition/RecognitionProcessor.java
@@ -0,0 +1,49 @@
+package com.paktalin.receiptanalyzer.recognition;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.text.FirebaseVisionCloudDocumentTextDetector;
+import com.google.firebase.ml.vision.cloud.text.FirebaseVisionCloudText;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.util.List;
+
+public class RecognitionProcessor extends VisionProcessorBase<FirebaseVisionCloudText> {
+
+    private static final String TAG = "CloudDocTextRecProc";
+
+    private final FirebaseVisionCloudDocumentTextDetector detector;
+
+    public RecognitionProcessor() {
+        super();
+        detector = FirebaseVision.getInstance().getVisionCloudDocumentTextDetector();
+    }
+
+    @Override
+    protected Task<FirebaseVisionCloudText> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @NonNull FirebaseVisionCloudText text,
+            @NonNull FrameMetadata frameMetadata) {
+        Log.d(TAG, "detected text is: " + text.getText());
+        for (FirebaseVisionCloudText.Page page: text.getPages()){
+            List<FirebaseVisionCloudText.DetectedLanguage> languages = page.getTextProperty().getDetectedLanguages();
+            for (FirebaseVisionCloudText.DetectedLanguage language : languages) {
+                Log.d(TAG, "confidence: " + language.getConfidence());
+                Log.d(TAG, "code: " + language.getLanguageCode());
+            }
+        }
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.w(TAG, "Cloud Document Text detection failed." + e);
+    }
+}
+
---
+++ app/src/main/java/com/paktalin/receiptanalyzer/recognition/VisionProcessorBase.java
@@ -0,0 +1,107 @@
+package com.paktalin.receiptanalyzer.recognition;
+
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class VisionProcessorBase<T> implements VisionImageProcessor {
+
+    // Whether we should ignore process(). This is usually caused by feeding input data faster than
+    // the model can handle.
+    private final AtomicBoolean shouldThrottle = new AtomicBoolean(false);
+
+    public VisionProcessorBase() {
+    }
+
+    @Override
+    public void process(
+            ByteBuffer data, final FrameMetadata frameMetadata) {
+        if (shouldThrottle.get()) {
+            return;
+        }
+        FirebaseVisionImageMetadata metadata =
+                new FirebaseVisionImageMetadata.Builder()
+                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                        .setWidth(frameMetadata.getWidth())
+                        .setHeight(frameMetadata.getHeight())
+                        .setRotation(frameMetadata.getRotation())
+                        .build();
+
+        detectInVisionImage(
+                FirebaseVisionImage.fromByteBuffer(data, metadata), frameMetadata);
+    }
+
+    // Bitmap version
+    @Override
+    public void process(Bitmap bitmap) {
+        if (shouldThrottle.get()) {
+            return;
+        }
+        detectInVisionImage(FirebaseVisionImage.fromBitmap(bitmap), null);
+    }
+
+    /**
+     * Detects feature from given media.Image
+     *
+     * @return created FirebaseVisionImage
+     */
+    @Override
+    public void process(Image image, int rotation) {
+        if (shouldThrottle.get()) {
+            return;
+        }
+        // This is for overlay display's usage
+        FrameMetadata frameMetadata =
+                new FrameMetadata.Builder().setWidth(image.getWidth()).setHeight(image.getHeight
+                        ()).build();
+        FirebaseVisionImage fbVisionImage =
+                FirebaseVisionImage.fromMediaImage(image, rotation);
+        detectInVisionImage(fbVisionImage, frameMetadata);
+    }
+
+    private void detectInVisionImage(
+            FirebaseVisionImage image,
+            final FrameMetadata metadata) {
+        detectInImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<T>() {
+                            @Override
+                            public void onSuccess(T results) {
+                                shouldThrottle.set(false);
+                                VisionProcessorBase.this.onSuccess(results, metadata);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                shouldThrottle.set(false);
+                                VisionProcessorBase.this.onFailure(e);
+                            }
+                        });
+        // Begin throttling until this frame of input has been processed, either in onSuccess or
+        // onFailure.
+        shouldThrottle.set(true);
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    protected abstract Task<T> detectInImage(FirebaseVisionImage image);
+
+    protected abstract void onSuccess(
+            @NonNull T results,
+            @NonNull FrameMetadata frameMetadata);
+
+    protected abstract void onFailure(@NonNull Exception e);
+}
